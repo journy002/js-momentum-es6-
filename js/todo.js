1c194b30a964dd187111eeb3358135963bdb1ec6
journy002 <journy002@naver.com>
2021-12-27 23:35:01 +0900

    update todos

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,13 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+const todos = [];
+
+function saveTodos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
 function deleteTodo(event) {
     const target = event.target;
     const li = target.parentNode;
@@ -24,7 +31,21 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
+    todos.push(newTodo);
     paintTodo(newTodo);
+    saveTodos();
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit);
+
+function sayHello(item) {
+    console.log("good", item);
+}
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log(savedToDos, "out");
+if (saveTodos) {
+    const parsedToDos = JSON.parse(savedToDos);
+    // parsedToDos.forEach(sayHello); 함수를 만들어서 넣는 방법도 있다. //
+    parsedToDos.forEach((item) => console.log(item));
+}
