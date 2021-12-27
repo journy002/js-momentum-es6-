e3d4d24c421e96f9310f582ccf08acbf3dc48322
journy002 <journy002@naver.com>
2021-12-27 18:46:26 +0900

    two case of js file

--- main.js
+++ main.js
@@ -5,15 +5,32 @@ const loginInput = loginForm.querySelector("input");
 const greeting = document.getElementById("greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
+// main.js 에선 paintGreetings() 함수에서 localstorage의 value를 찾고
+// savedUsername 변수를 만들때도 찾는다.
+// 유저정보를 두번 찾는다는 의미 이다.
+// 이렇게 해도 되지만 굳이 이럴 필요가 없기에 그 코드는 test.js를 보면 알 수 있다.
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem("username", username);
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-    console.log(username);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreetings();
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings() {
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greetings
+    paintGreetings();
+}
---
+++ test.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const greetings = document.getElementById("gretting");
+
+const HIDDEN_USERNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function handleLoginClick(e) {
+    e.preventDefault();
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    loginForm.classList.add(HIDDEN_USERNAME);
+    printGreetings(username);
+    // greeting.classList.remove(HIDDEN_USERNAME);
+    // greeting.innerText = `Hello ${username}!`;
+}
+
+function printGreetings(username) {
+    greeting.classList.remove(HIDDEN_USERNAME);
+    greeting.innerText = `Hello ${username}!`;
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_USERNAME);
+    loginForm.addEventListener("submit", handleLoginClick);
+} else {
+    printGreetings(savedUsername);
+}
